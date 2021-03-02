16623569d432406f4497781aa4cc2e19ed90c11a
Fl0rk3 <55188734+Fl0rk3@users.noreply.github.com>
2021-03-02 01:18:11 +0100

    Add files via upload

---
+++ scripts/slide.js
@@ -0,0 +1,24 @@
+new Splide('.splide_house',
+    {
+        type: 'loop'
+    }).mount();
+
+new Splide('.splide_bedroom',
+    {
+        type: 'loop'
+    }).mount();
+
+new Splide('.splide_living',
+    {
+        type: 'loop'
+    }).mount();
+
+new Splide('.splide_kitchen',
+    {
+        type: 'loop'
+    }).mount();
+
+new Splide('.splide_bathroom',
+    {
+        type: 'loop'
+    }).mount();
